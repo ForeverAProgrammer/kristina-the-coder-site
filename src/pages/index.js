48bb56612531c350dc9644d04d75a88b0b2544f2
KiraRenee <kristina.haynes@hotmail.com>
2020-08-02 21:10:55 -0500

    Customizing UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,27 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import { Helmet } from 'react-helmet'
-import Hero from '../components/hero'
 import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import BlogPreview from '../components/blog-preview'
+import SiteBanner from '../components/site-banner'
 
 class RootIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    const [siteBanner] = get(this, 'props.data.allContentfulBanner.edges')
 
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <Hero data={author.node} />
+          <Helmet title="Kristina Codes" />
+          <SiteBanner  data = {siteBanner.node}/>
           <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
+            <h2 className="section-headline">Recent posts</h2>
+            <ul className="post-list">
               {posts.map(({ node }) => {
                 return (
                   <li key={node.slug}>
-                    <ArticlePreview article={node} />
+                    <BlogPreview post={node} />
                   </li>
                 )
               })}
@@ -39,18 +38,28 @@ export default RootIndex
 
 export const pageQuery = graphql`
   query HomeQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+    allContentfulBanner(limit: 1) {
+      edges {
+        node {
+          id
+          siteTitle
+          siteSubTitle
+          banner {
+            fluid(resizingBehavior: FILL) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+            title
+          }
+        }
+      }
+    }
+    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }, limit: 3) {
       edges {
         node {
           title
           slug
           publishDate(formatString: "MMMM Do, YYYY")
           tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
           description {
             childMarkdownRemark {
               html
